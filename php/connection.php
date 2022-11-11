3a4df5ab2755707319c075b75b2151563050fbbb
tisnahadiana <77492139+tisnahadiana@users.noreply.github.com>
2022-11-11 21:24:49 +0700

    Delete php directory

--- php/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $connect =mysqli_connect("localhost","u606784881_hadiana","@Hasan90256801","u606784881_sia");
-    if ($connect){
-        echo"Connection Estabilished";
-    } else {
-        echo "Connection Error";
-    }
-?>
